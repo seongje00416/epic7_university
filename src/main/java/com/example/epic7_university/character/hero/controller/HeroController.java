7925bbfaa3bde63ff63993fe7daaedbe522d03e2
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-12-17 20:44:19 +0900

    feat: 영웅 관련 API 제작중

--- src/main/java/com/example/epic7_university/character/hero/controller/HeroController.java
+++ src/main/java/com/example/epic7_university/character/hero/controller/HeroController.java
@@ -1,6 +1,8 @@
 package com.example.epic7_university.character.hero.controller;
 
+import com.example.epic7_university.character.hero.dto.response.GetAllHeroResponse;
 import com.example.epic7_university.character.hero.dto.response.GetHeroDetailResponse;
+import com.example.epic7_university.common.dto.PageResponse;
 import com.example.epic7_university.common.dto.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -12,6 +14,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag( name = "Hero API", description = "영웅 API" )
 public interface HeroController {
+    @Operation( summary = "영웅 상세 조회", description = "영웅 상세 조회 API" )
+    @ApiResponses( value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "조회 성공"
+            )
+    })
+    @GetMapping( value = "/getHero/{heroId}" )
+    ResponseEntity<SuccessResponse<GetHeroDetailResponse>> getHeroDetail(
+            @RequestParam Long heroId
+    );
+
     @Operation( summary = "영웅 전체 조회", description = "영웅 전체 조회 API" )
     @ApiResponses( value = {
             @ApiResponse(
@@ -19,8 +33,6 @@ public interface HeroController {
                     description = "조회 성공"
             )
     })
-    @GetMapping( value = "/{heroId}" )
-    ResponseEntity<SuccessResponse<GetHeroDetailResponse>> getHeroDetail(
-            @RequestParam Long heroId
-    );
+    @GetMapping( value = "/getAllHero" )
+    ResponseEntity<SuccessResponse<PageResponse<GetAllHeroResponse>>> getAllHero();
 }
